589a30491829aa78ec2cd7f01ef39992e6d69652
catseye <cpressey@gmail.com>
2011-08-25 11:54:58 -0500

    Add a Python example demonstrating usage of mkstemp.

---
+++ python/mkstemp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# A gotcha with mkstemp: see e.g. http://www.logilab.org/blogentry/17873
+
+import os
+from tempfile import mkstemp
+
+fd, filename = mkstemp()
+with open(filename, 'w') as file:
+    file.write('hello')
+    file.close()
+os.close(fd) # if you do not do this, you will leak filehandles
+print "wrote out %s" % filename
