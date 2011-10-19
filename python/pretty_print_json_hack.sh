0e79639a41005ce6a288cf69ab042cf8da501a1e
catseye <cpressey@gmail.com>
2011-10-19 15:41:20 -0500

    Add two Python command-line hacks.

---
+++ python/pretty_print_json_hack.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# pipe JSON into this to pretty-print it.
+# see: http://stackoverflow.com/questions/352098/how-to-pretty-print-json-script
+
+python -mjson.tool
---
+++ python/simple_webserver_hack.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# starts a simple webserver for the current directory on interface 0.0.0.0 port 8000 
+# see http://www.linuxjournal.com/content/tech-tip-really-simple-http-server-python
+
+python -m SimpleHTTPServer
