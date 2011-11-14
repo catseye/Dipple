4db650268bea223319e7515b55bde6c7404b597e
Chris Pressey <cpressey@gmail.com>
2011-11-13 20:23:42 -0600

    Add more efficient version.

--- haskell/PascalsTriangle.hs
+++ haskell/PascalsTriangle.hs
@@ -1,6 +1,7 @@
 --
 -- Naive Lazy Pascal's Triangle calculator.  It's inefficient enough that
--- you can just type 'pascalsTriangle' at the ghci prompt, and watch it grow!
+-- you can just type 'pascalsTriangle' at the ghci prompt, and watch it chug
+-- along!
 --
 
 pascal row 1 = 1
@@ -16,3 +17,21 @@ pascalsTriangle = [[pascal row col | col <- [1..row]] | row <- [1..]]
 demo = take 10 pascalsTriangle
 
 outOfBounds = error "Out of bounds"
+
+--
+-- A less naive, and generally weirder, version.  Unlike pascalsTriangle,
+-- this implementation WILL start quickly scrolling off your screen if
+-- you just type 'paTri' at the ghci prompt.
+--
+
+paTri = paTri' 1
+paTri' 1 = [1]:(paTri' 2)
+paTri' 2 = [1,1]:(paTri' 3)
+paTri' n =
+    [gorb x | x <- [1..n]]:(paTri' (n+1))
+    where
+        prevRow = head $ paTri' (n-1)
+        gorb 1 = 1
+        gorb col
+            | col == n = 1
+            | otherwise = (prevRow !! (col-2)) + (prevRow !! (col-1))
