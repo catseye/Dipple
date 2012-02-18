0b4f6f6649c1ea0fa44f35ebae2719b4dfd05ff5
catseye <cpressey@gmail.com>
2012-02-18 11:49:14 -0600

    Add rudimentary dotfile stuff for bash.

---
+++ bash/dot_bashrc.sh
@@ -0,0 +1,9 @@
+# Not a fully formed script; intended to be copied into, or sourced from,
+# .bashrc or .bash_profile.
+# Also woefully incomplete right now.
+
+export EDITOR=nano
+export PATH="$HOME/bin:$PATH"
+
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
