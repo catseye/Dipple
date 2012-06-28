dc765a48dfc1eca1d1488e07f09c5502b8034928
Cat's Eye Technologies <cpressey@gmail.com>
2012-06-28 15:25:20 -0700

    Make even poorer, so to speak.

--- python/poor-mans-yaml-front-matter.py
+++ python/poor-mans-yaml-front-matter.py
@@ -7,21 +7,26 @@ Poor-man's YAML Front Matter.  Inspired by jekyll's YAML Front Matter:
 
 ...except done in a much more chewing-gum-and-baling-wire fashion.
 
-You write your document as a YAML file, but you include its body, as
-a Markdown document, under the `document.body` key in the YAML.
-You can do this with a YAML block scalar; the only downside is that
-you have to indent the whole Markdown document.  (Not a huge tragedy
-with a half-decent text editor that can indent and unindent for you)
+You write your document as a YAML file, but you include its body, as a
+Markdown document, under the `body` key in the YAML.
 
-The contents of the `document` key in the YAML is made available as
-a template context for rendering the document; you can include Jinja2
-("moustache")-style variables in your Markdown, and these will be
-replaced by the various values in the YAML file.
+You can do this with a YAML block scalar; the only downside is that you
+have to indent the whole Markdown document two spaces.  (Not a huge
+tragedy when you have a half-decent text editor at your disposal)
 
-You can also specify a template file under the `document.template`
-key; that will load the file with that name and render it with the
-same context (except that `body` now refers to the HTML that was
-rendered from the Markdown.)
+The contents of the `document` key in the YAML is made available as a
+template context for rendering the document; you can include Jinja2
+("moustache"-style) variables in your Markdown, and these will be replaced
+by the various values in the YAML file.
+
+You can also specify a template file under the `template` key; that will
+load the template file with that name and render it with the same context,
+except with `body` now referring to the HTML that was rendered from the
+Markdown.
+
+There are of course scores of variations on this theme; your YAML file
+could have a list or dictionary of such documents, the text format need
+not be Markdown, the templating engine need not be Jinja2, etc., etc.
 
 Try it:
 
@@ -44,15 +49,14 @@ def render(filename):
     file = open(filename)
     data = yaml.load(file, Loader=Loader)
     file.close()
-    document = data['document']
-    html = markdown.markdown(document['body'])
+    html = markdown.markdown(data['body'])
     template = jinja2.Template(html)
-    body = template.render(document)
-    document['body'] = body
-    file = open(document['template'])
+    body = template.render(data)
+    data['body'] = body
+    file = open(data['template'])
     template = jinja2.Template(file.read())
     file.close()
-    body = template.render(document)
+    body = template.render(data)
     return body
 
 
