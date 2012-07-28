f3f54440886ee1a4b975156127480d1bac371a7d
catseye <cpressey@gmail.com>
2012-07-28 11:24:07 -0500

    Fix implementation and tests.

--- python/room.py
+++ python/room.py
@@ -2,7 +2,10 @@
 # more efficiently.
 
 class Room(object):
-    """
+    """A square grid of characters, which may be virtually rotated.
+    orientation is an integer indicating the amount of rotation, in degrees,
+    clockwise from the standard orientation.  It must be one of the following
+    values: 0, 90, 180, or 270.
     
     >>> r = Room(3, 0)
     >>> r.store(0, 0, 'a')
@@ -18,21 +21,21 @@ class Room(object):
 
     >>> r.orientation = 90
     >>> print r
-    c Z
-    b  
-    a-A
+    A-a
+      b
+    Z c
 
     >>> r.orientation = 180
     >>> print r
     Z A
-      - 
+      -
     cba
 
     >>> r.orientation = 270
     >>> print r
-    A-a
-      b
-    Z c
+    c Z
+    b  
+    a-A
 
     """
     def __init__(self, size, orientation):
@@ -46,9 +49,9 @@ class Room(object):
         elif self.orientation == 90:
             return (y, (self.size-1) - x)
         elif self.orientation == 180:
-            return ((self.size-1) - x, y)
+            return ((self.size-1) - x, (self.size-1) - y)
         elif self.orientation == 270:
-            return (y, x)
+            return ((self.size-1) - y, x)
         else:
             raise NotImplementedError("Orientation must be 0, 90, 180, or 270")
 
