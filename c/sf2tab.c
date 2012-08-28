b13d6fb588111039f7ef804fc9ffbaec1b296147
catseye <cpressey@gmail.com>
2012-08-27 20:23:52 -0500

    Place three of my programs in the public domain.

--- c/sf2tab.c
+++ c/sf2tab.c
@@ -1,44 +1,12 @@
 /*
- * Copyright (c)2005 Cat's Eye Technologies.  All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 
- *   Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- * 
- *   Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in
- *   the documentation and/or other materials provided with the
- *   distribution.
- * 
- *   Neither the name of Cat's Eye Technologies nor the names of its
- *   contributors may be used to endorse or promote products derived
- *   from this software without specific prior written permission. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE. 
- */
-
-/*
  * sf2tab.c
  * Smallfuck to lookup-table compiler.
- * $Id$
+ * By Chris Pressey, Cat's Eye Technologies, circa 2005.
+ * This work has been placed in the public domain.
  */
 
 /*
- * This compiler takes programs written in the Smallf*ck programming language
+ * This compiler takes programs written in the Smallfuck programming language
  * and converts them to equivalent lookup tables.  This was written mainly to
  * demonstrate what happens when your language is defined over a finite
  * state-space: every program can be run in O(n) time!
