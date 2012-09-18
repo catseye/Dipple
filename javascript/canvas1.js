6fee93ff9b8bb1b83265e6aed93c3f9aa257ae82
catseye <cpressey@gmail.com>
2012-09-17 23:26:10 -0500

    Let's clean these up a little bit.

--- javascript/canvas1.js
+++ javascript/canvas1.js
@@ -1,37 +1,36 @@
-var ctx;
-var counter;
-var interval_id;
+function CanvasExperiment1(canvas) {
+    var self = {};
+    var counter;
+    var interval_id;
 
-function draw() {
-  var canvas = document.getElementById("canvas");
-  ctx = canvas.getContext('2d');
+    self.draw = function() {
+      var ctx = canvas.getContext('2d');
 
-  $('#counter').html(counter);
+      $('#counter').html(counter);
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.beginPath();
-  ctx.lineWidth = "1";
-  ctx.strokeStyle = "black";
-  ctx.moveTo(0,0);
-  var y = 200;
-  var w = (canvas.width / counter);
-  for (var i = 1; i <= counter; i++) {
-      ctx.lineTo(w * i, y);
-      y = (y == 200 ? 0 : 200);
-  }
-  ctx.lineTo(canvas.width,0);
-  ctx.stroke();
-  
-  counter += 1;
-}  
+      ctx.beginPath();
+      ctx.lineWidth = "1";
+      ctx.strokeStyle = "black";
+      ctx.moveTo(0,0);
+      var y = 200;
+      var w = (canvas.width / counter);
+      for (var i = 1; i <= counter; i++) {
+          ctx.lineTo(w * i, y);
+          y = (y == 200 ? 0 : 200);
+      }
+      ctx.lineTo(canvas.width,0);
+      ctx.stroke();
+      
+      counter += 1;
+    };
 
-function start() {
-  counter = 1;
-  draw();
-  interval_id = setInterval(draw, 50);
+    self.start = function() {
+      counter = 1;
+      self.draw();
+      interval_id = setInterval(self.draw, 50);
+    };
+
+    return self;
 }
-
-$(document).ready(function() {
-  start();
-});
--- javascript/canvas2.js
+++ javascript/canvas2.js
@@ -1,41 +1,40 @@
-var ctx;
-var p;
-var interval_id;
-var w = 40;
-var h = 40;
+function CanvasExperiment2(canvas) {
+    var self = {};
+    var p;
+    var interval_id;
+    var w = 40;
+    var h = 40;
 
-var colors = ["red", "black", "green", "blue"];
+    var colors = ["red", "black", "green", "blue"];
 
-function draw() {
-    var canvas = document.getElementById("canvas");
-    ctx = canvas.getContext('2d');
+    self.draw = function() {
+        var ctx = canvas.getContext('2d');
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    var c = colors[Math.floor(Math.random() * 4)]
-    var x = Math.floor(Math.random() * 10)
-    var y = Math.floor(Math.random() * 10)
-    
-    if (Math.random() > 0.25) {
-        p.put(x, y, c);
-    } else {
-        p.put(x, y, undefined);
+        var c = colors[Math.floor(Math.random() * 4)]
+        var x = Math.floor(Math.random() * 10)
+        var y = Math.floor(Math.random() * 10)
+        
+        if (Math.random() > 0.25) {
+            p.put(x, y, c);
+        } else {
+            p.put(x, y, undefined);
+        }
+
+        p.foreach(function (x, y, value) {
+            ctx.beginPath();
+            ctx.fillStyle = value;
+            ctx.arc(x * w + (w / 2), y * h + (h / 2), w/2, 0, 2 * Math.PI, false);
+            ctx.fill();
+        });
     }
 
-    p.foreach(function (x, y, value) {
-        ctx.beginPath();
-        ctx.fillStyle = value;
-        ctx.arc(x * w + (w / 2), y * h + (h / 2), w/2, 0, 2 * Math.PI, false);
-        ctx.fill();
-    });
-}  
+    self.start = function() {
+        p = Playfield();
+        self.draw();
+        interval_id = setInterval(self.draw, 25);
+    }
 
-function start() {
-    p = Playfield();
-    draw();
-    interval_id = setInterval(draw, 25);
+    return self;
 }
-
-$(document).ready(function() {
-    start();
-});
