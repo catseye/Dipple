c4fdb16d450b2732b1334b60bad9dd96d9bcaf40
catseye <cpressey@gmail.com>
2012-12-22 21:31:56 +0000

    These have part-time jobs in my dotfiles now.

--- python/pretty_print_json_hack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# pipe JSON into this to pretty-print it.
-# see: http://stackoverflow.com/questions/352098/how-to-pretty-print-json-script
-
-# Contains code from a website, and is therefore not in the public domain.
-
-python -mjson.tool
--- python/simple_webserver_hack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# starts a simple webserver for the current directory on interface 0.0.0.0 port 8000 
-# see http://www.linuxjournal.com/content/tech-tip-really-simple-http-server-python
-
-# Contains code from a blog, and is therefore not in the public domain.
-
-python -m SimpleHTTPServer
