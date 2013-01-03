65f214c57bb28b0e260ca42e3687629cebded593
catseye <cpressey@gmail.com>
2013-01-03 14:02:12 +0000

    Make OO more JS-y.  Add local copy of (public domain) image.

--- javascript/canvas5.js
+++ javascript/canvas5.js
@@ -1,5 +1,5 @@
-function Canvas5Controller(canvas) {
-    var self = {};
+Canvas5Controller = function() {
+    var canvas;
     var interval_id;
 
     var img = new Image();
@@ -15,7 +15,7 @@ function Canvas5Controller(canvas) {
         shapes[i].alpha = Math.random() * 0.66;
     }
 
-    self.draw = function() {
+    this.draw = function() {
         var ctx = canvas.getContext('2d');
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -37,24 +37,25 @@ function Canvas5Controller(canvas) {
         }
     }
 
-    self.start = function() {
+    this.start = function(c) {
+        canvas = c;
         for (var i = 0; i < NUM_SHAPES; i++) {
             shapes[i] = {};
             new_shape(i);
         }
+        var self = this;
         img.onload = function() {
             self.draw();
             interval_id = setInterval(self.draw, 20);
         }
-        img.src = 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Elevator_1_%28PSF%29.png/640px-Elevator_1_%28PSF%29.png';
+        // http://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Elevator_1_%28PSF%29.png/640px-Elevator_1_%28PSF%29.png
+        img.src = 'Elevator_1_(PSF).png';
     }
 
-    self.stop = function() {
+    this.stop = function() {
         if (interval_id === undefined)
             return;
         clearInterval(interval_id);
         interval_id = undefined;
     }
-
-    return self;
 }
