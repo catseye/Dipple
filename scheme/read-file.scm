4fcbd130c31cedb69333addc5e4cb8fbb9ddee1e
Chris Pressey <cpressey@catseye.tc>
2014-12-06 10:30:25 +0000

    Add a rudimentary tokenizer.

--- scheme/read-file.scm
+++ scheme/read-file.scm
@@ -1,4 +1,3 @@
-
 (define read-chars
   (lambda (port acc)
     (let* ((c (read-char port)))
@@ -7,6 +6,41 @@
         (else            (read-chars port (cons c acc)))))))
 
 (define read-file
-  (lambda (name)
-    (let* ((port (open-input-file name)))
+  (lambda (filename)
+    (let* ((port (open-input-file filename)))
       (read-chars port '()))))
+
+(define whitespace?
+  (lambda (char)
+    (or (equal? char #\space)
+        (equal? char #\newline)
+        (equal? char #\tab))))
+
+(define extract-whitespace
+  (lambda (chars acc)
+    (if (null? chars) (list (reverse acc) chars)
+      (let* ((char (car chars))
+             (rest (cdr chars)))
+        (if (whitespace? char)
+          (extract-whitespace rest (cons char acc))
+          (list (reverse acc) chars))))))
+
+(define extract-word
+  (lambda (chars acc)
+    (if (null? chars) (list (reverse acc) chars)
+      (let* ((char (car chars))
+             (rest (cdr chars)))
+        (if (whitespace? char)
+          (list (reverse acc) chars)
+          (extract-word rest (cons char acc)))))))
+
+(define extract-words
+  (lambda (chars acc)
+    (if (null? chars) (reverse acc)
+      (let* ((result  (extract-word chars '()))
+             (word    (list->string (car result)))
+             (rest    (cadr result))
+             (result2 (extract-whitespace rest '()))
+             (word2   (list->string (car result2)))
+             (rest2   (cadr result2)))
+        (extract-words rest2 (cons word acc))))))
