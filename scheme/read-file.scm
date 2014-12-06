8466fd83f8e81fcbe9ebe4ac066559393570b3c8
Chris Pressey <cpressey@catseye.tc>
2014-12-06 11:44:51 +0000

    Some sketching has certainly occurred.

--- scheme/read-file.scm
+++ scheme/read-file.scm
@@ -1,16 +1,14 @@
 ; Much sketching must occur.
 
-(define read-chars
-  (lambda (port acc)
-    (let* ((c (read-char port)))
-      (cond
-        ((eof-object? c) (reverse acc))
-        (else            (read-chars port (cons c acc)))))))
+; This module works on several kinds of "tagged list structures":
 
-(define read-file
-  (lambda (filename)
-    (let* ((port (open-input-file filename)))
-      (read-chars port '()))))
+; (chars ...) -- a list of Scheme character objects representing a text
+; (words ...) -- a list of "chars"es, where each "chars" is a word OR whitespace chunk
+; (lines ...) -- a list of "chars"es, where each "chars" is a line
+
+;
+; Utilties -- ignore these for now -- skip down for the good stuff
+;
 
 (define whitespace?
   (lambda (char)
@@ -27,6 +25,48 @@
     (if (null? chars) chars)
       (if (pred (car chars) (cdr chars))
         chars)))
+
+;
+; Open and read a text file as a "chars".
+;
+(define read-file
+  (lambda (filename)
+    (let* ((port (open-input-file filename)))
+      (read-chars port '()))))
+
+(define read-chars
+  (lambda (port acc)
+    (let* ((c (read-char port)))
+      (cond
+        ((eof-object? c) (cons 'chars (reverse acc)))
+        (else            (read-chars port (cons c acc)))))))
+
+;
+; Regroup a "chars" into a "lines" of "chars".
+;
+(define extract-lines
+  (lambda (chars)
+    (if (equal? (car chars) 'chars)
+      (extract-lines-rec (cdr chars) '()))))
+
+(define extract-lines-rec
+  (lambda (chars acc)
+    (if (null? chars) (cons 'lines (reverse acc))
+      (let* ((result  (extract-line chars '()))
+             (line    (car result))
+             (rest    (cadr result)))
+        (extract-lines-rec rest (cons line acc))))))
+
+(define extract-line
+  (lambda (chars acc)
+    (if (null? chars) (list (cons 'char (reverse acc)) chars)
+      (let* ((char (car chars))
+             (rest (cdr chars)))
+        (if (newline? char)
+          (list (cons 'char (reverse acc)) rest)
+          (extract-line rest (cons char acc)))))))
+
+; --------- not converted out yet -------- ;
 
 (define extract-whitespace
   (lambda (chars acc)
@@ -46,15 +86,6 @@
           (list (reverse acc) chars)
           (extract-word rest (cons char acc)))))))
 
-; ----- untagged lists -----
-
-; (#\a #\b ...) -- a list of chars representing a text (or portion thereof)
-
-; ----- tagged-list style ----- ;
-
-; (lines ...)  -- a list of strings, where each string is a line
-; (words ...)  -- a list of strings, where each string is a word
-
 (define extract-words
   (lambda (chars acc)
     (if (null? chars) (cons 'words (reverse acc))
@@ -65,21 +96,3 @@
              (word2   (list->string (car result2)))
              (rest2   (cadr result2)))
         (extract-words rest2 (cons word2 (cons word acc)))))))
-
-(define extract-line
-  (lambda (chars acc)
-    (if (null? chars) (list (reverse acc) chars)
-      (let* ((char (car chars))
-             (rest (cdr chars)))
-        (if (newline? char)
-          (list (reverse acc) rest)
-          (extract-line rest (cons char acc)))))))
-
-(define extract-lines
-  (lambda (chars acc)
-    (if (null? chars) (cons 'lines (reverse acc))
-      (let* ((result  (extract-line chars '()))
-             (line    (list->string (car result)))
-             (rest    (cadr result)))
-        (extract-lines rest (cons line acc))))))
-
