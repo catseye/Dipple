cc1c9749e080de61c22f62d208ac70e6865411aa
Chris Pressey <cpressey@catseye.tc>
2019-01-12 22:10:54 +0000

    Just a quick reminder of Haskell's record syntax that I never use.

---
+++ haskell/record.hs
@@ -0,0 +1,15 @@
+data Product = Product {
+        desc :: String,
+        sku :: String,
+        qty :: Int,
+        weight :: Float
+    } deriving (Show)
+
+
+demo =
+   let
+       p = Product{ desc="White paint", sku="PW00", qty=1, weight=14.4 }
+       p' = p { qty=100 }
+       q = weight p'
+   in
+       show (p', q)
